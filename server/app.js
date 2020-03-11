6f31f3fca8dca33f6da0800e2bc71a817870f92b
chenshenhai <cshenhai@gmail.com>
2020-03-11 21:07:02 +0800

    feat: init static server

--- server/app.js
+++ server/app.js
@@ -1,8 +1,32 @@
+const path = require('path');
 const Koa = require('koa');
+const render = require('koa-ejs');
+const koaStatic = require('./middlewares/static');
 const app = new Koa();
 
-app.use((ctx) => {
-  ctx.body = 'hello book-reader';
+
+app.use(koaStatic(
+  path.join(__dirname , '..', 'static'),
+  {
+    prefix: '/dist/'
+  }
+))
+
+render(app, {
+  root: path.join(__dirname, 'view'),
+  layout: 'template',
+  viewExt: 'html',
+  cache: false,
+  // debug: true
+});
+
+app.use(async (ctx) => {
+  if (ctx.path === '/') {
+    await ctx.render('index', {
+      title: 'my-title',
+      name: 'my-name',
+    });
+  }
 });
 
 function startAsync() {
---
+++ server/middlewares/static.js
@@ -0,0 +1,75 @@
+// Copyright (c) 2019 Koa Static Contributors
+// https://github.com/koajs/static/blob/master/LICENSE
+
+'use strict'
+
+/**
+ * Module dependencies.
+ */
+
+const debug = require('debug')('koa-static')
+const { resolve } = require('path')
+const assert = require('assert')
+const send = require('koa-send')
+
+/**
+ * Expose `serve()`.
+ */
+
+module.exports = serve
+
+/**
+ * Serve static files from `root`.
+ *
+ * @param {String} root
+ * @param {Object} [opts]
+ * @return {Function}
+ * @api public
+ */
+
+function serve (root, opts) {
+  opts = Object.assign({}, opts)
+
+  assert(root, 'root directory is required to serve files')
+
+  // options
+  debug('static "%s" %j', root, opts)
+  opts.root = resolve(root)
+  if (opts.index !== false) opts.index = opts.index || 'index.html'
+
+  if (!opts.defer) {
+    return async function serve (ctx, next) {
+      let done = false
+
+      if (ctx.path.startsWith(opts.prefix || '/') && (ctx.method === 'HEAD' || ctx.method === 'GET')) {
+        try {
+          done = await send(ctx, ctx.path, opts)
+        } catch (err) {
+          if (err.status !== 404) {
+            throw err
+          }
+        }
+      }
+
+      if (!done) {
+        await next()
+      }
+    }
+  }
+
+  return async function serve (ctx, next) {
+    await next()
+
+    if (ctx.method !== 'HEAD' && ctx.method !== 'GET' && !ctx.path.startsWith(opts.prefix || '/')) return
+    // response is already handled
+    if (ctx.body != null || ctx.status !== 404) return // eslint-disable-line
+
+    try {
+      await send(ctx, ctx.path, opts)
+    } catch (err) {
+      if (err.status !== 404) {
+        throw err
+      }
+    }
+  }
+}
