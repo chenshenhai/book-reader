cc3c6eaa907d67d1361580722fd9687bb9664abc
chenshenhai <cshenhai@gmail.com>
2020-03-12 21:52:41 +0800

    feat: add gzip build

--- static/script/webpack.prod.config.js
+++ static/script/webpack.prod.config.js
@@ -3,13 +3,14 @@ process.env.NODE_ENV = 'production';
 const merge = require('webpack-merge');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 const config = require('./webpack.base.config');
 
 module.exports = merge(config, {
   mode: 'production',
-  // plugins: [
-  //   new UglifyJsPlugin()
-  // ]
+  plugins: [
+    new CompressionPlugin()
+  ],
   optimization: {
     minimizer: [
       new UglifyJsPlugin({
