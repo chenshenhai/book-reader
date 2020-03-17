421d81175ea2b75f0a13aee2bebb6e8fa79ec02e
chenshenhai <cshenhai@gmail.com>
2020-03-17 23:23:47 +0800

    feat: update url

--- static/src/js/lib/url.js
+++ static/src/js/lib/url.js
@@ -5,12 +5,19 @@ function parseToInnerPath(url) {
   let pagePath = url;
   const config = viewConfig.getData();
   const regFullFile = new RegExp(`^${config.srcSite}\/${config.srcDev}\/(.*)\.md$`, 'i');
+  const regSrcBaseUrl  = new RegExp(`^${config.srcSite}\/${config.srcDev}\/`, 'i');
   pagePath = pagePath.replace(/^https\:\/\//, '').replace(/^http\:\/\//, '').replace(/^\/\//, '').replace(/^www./)
   let bookName = config.currentBook || '';
+
   if (url.startsWith('./') === true && url.startsWith(`./${bookName}/`) !== true) {
     return pagePath.replace('./', `/${bookName}/`).replace(/\.md$/, '');
   } else if (regFullFile.test(pagePath)) {
     return pagePath.replace(regSrcBaseUrl, '/').replace('/blob/master/', '/').replace(/\.md$/, '');
+  } else if (regSrcBaseUrl.test(pagePath)) {
+    pagePath = pagePath.replace(regSrcBaseUrl, '/').replace(/\/$/, '');
+    if (/^\/[0-9a-zA-Z\-\_]{1,}$/.test(pagePath)) {
+      return pagePath;
+    }
   }
   return url;
 }
