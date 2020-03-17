de78dbc8401545a648b9d9dc58a85a7075f09480
chenshenhai <cshenhai@gmail.com>
2020-03-17 23:15:21 +0800

    feat: update url

--- static/src/js/lib/url.js
+++ static/src/js/lib/url.js
@@ -4,15 +4,12 @@ import { viewConfig } from './view';
 function parseToInnerPath(url) {
   let pagePath = url;
   const config = viewConfig.getData();
-  const regSrcBaseUrl = new RegExp(`^${config.srcSite}\/${config.srcDev}\/`, 'i');
   const regFullFile = new RegExp(`^${config.srcSite}\/${config.srcDev}\/(.*)\.md$`, 'i');
   pagePath = pagePath.replace(/^https\:\/\//, '').replace(/^http\:\/\//, '').replace(/^\/\//, '').replace(/^www./)
   let bookName = config.currentBook || '';
   if (url.startsWith('./') === true && url.startsWith(`./${bookName}/`) !== true) {
     return pagePath.replace('./', `/${bookName}/`).replace(/\.md$/, '');
   } else if (regFullFile.test(pagePath)) {
-    return pagePath.replace(regSrcBaseUrl, '/').replace('/blob/master/', '/').replace(/\.md$/, '');
-  } else if (regSrcBaseUrl.test(pagePath)) {
     return pagePath.replace(regSrcBaseUrl, '/').replace('/blob/master/', '/').replace(/\.md$/, '');
   }
   return url;
@@ -30,18 +27,6 @@ function isInnerPageUrl(url) {
     result = true;
   } else if (/^\/[0-9a-zA-Z\_\-]{1,}/.test(url)) {
     result = true;
-  } else if (url.startsWith('//') || url.startsWith('https://') || url.startsWith('http://')) {
-    let baseUrl = url.replace(/^https\:\/\//, '').replace(/^http\:\/\//, '').replace(/\/\//, '').replace(/^www./);
-    baseUrl = baseUrl.split('?')[0] || '';
-    const itemList = baseUrl.split('/');
-    const site = itemList[0];
-    const dev = itemList[1];
-    const book = itemList[2];
-    if ([config.srcSite].indexOf(site) >= 0 && config.srcDev === dev && config.books.indexOf(book) >= 0) {
-      if (url.endsWith('.md') || itemList.length === 3) {
-        result = true;
-      }
-    }
   }
   return result;
 }
