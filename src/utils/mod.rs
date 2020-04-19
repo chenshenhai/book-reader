82f516a13ceb0d6bb3618da15c365815dbaaff6a
chenshenhai <cshenhai@gmail.com>
2020-04-19 08:18:05 +0800

    feat: update rust server

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -28,6 +28,7 @@ pub mod config {
         pub wechat: String,
     }
 
+    #[allow(non_snake_case)]
     #[derive(Serialize, Deserialize, Debug)]
     pub struct Config {
         pub name: String,
